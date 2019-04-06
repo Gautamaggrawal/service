6aa937a1c5649b21ef63fef6c8ccbd4115cf025f
Gautam <gautamaggrawalsd@yahoo.in>
2019-04-07 01:23:25 +0530

    added models

--- service/models.py
+++ service/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from .utils import *
 
-# Create your models here.
+class MyURL(models.Model):
+	url=models.CharField(max_length=220,)
+	shortcode=models.CharField(max_length=15,unique=True)
+	updated=models.DateTimeField(auto_now=True)
+	timestamp=models.DateTimeField(auto_now_add=True)
+
+	def save(self,*args,**kwargs):
+		if self.shortcode is None or self.shortcode=="":
+			self.shortcode=code_generator()
+		super(MyURL,self).save(*args,**kwargs)
+
+
+
+
+
+	def __str__(self):
+		return str(self.url)
+	def __unicode__(self):
+		return __str__(self.url)
